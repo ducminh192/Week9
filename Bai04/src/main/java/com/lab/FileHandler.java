0ff3b241811fa7ad03c5dc492378c6b280928f9c
ducminh192 <minhhvux07@gmail.com>
2026-05-07 22:52:24 +0700

    Bai 4: Test Matrix Strategy with hardcoded path

---
+++ Bai04/src/main/java/com/lab/FileHandler.java
@@ -0,0 +1,11 @@
+package com.lab;
+
+import java.io.File;
+
+public class FileHandler {
+
+    public String getSafeFilePath(String folder, String filename) {
+        // Cách an toàn: Luôn dùng File.separator để tự động sinh đúng dấu gạch chéo theo HĐH
+        return folder + "\\" + filename;
+    }
+}
