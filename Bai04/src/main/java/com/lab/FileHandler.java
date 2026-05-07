cad81bc4400d555985006d1e01eb8c60c252cbbb
ducminh192 <minhhvux07@gmail.com>
2026-05-07 22:54:30 +0700

    Bai 4: Test Matrix Strategy

--- Bai04/src/main/java/com/lab/FileHandler.java
+++ Bai04/src/main/java/com/lab/FileHandler.java
@@ -6,6 +6,6 @@ public class FileHandler {
 
     public String getSafeFilePath(String folder, String filename) {
         // Cách an toàn: Luôn dùng File.separator để tự động sinh đúng dấu gạch chéo theo HĐH
-        return folder + "\\" + filename;
+        return folder + File.separator + filename;
     }
 }
