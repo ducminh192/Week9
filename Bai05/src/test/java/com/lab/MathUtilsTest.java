ba7151bd4838352082118fa16f3f10cfb315c445
ducminh192 <minhhvux07@gmail.com>
2026-05-07 23:08:57 +0700

    Bai 05 : da sua

--- Bai05/src/test/java/com/lab/MathUtilsTest.java
+++ Bai05/src/test/java/com/lab/MathUtilsTest.java
@@ -4,12 +4,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathUtilsTest {
+
     @Test
-    void testSubtract() {
-        assertEquals(1, new MathUtils().subtract(3, 2));
-    }
-    @Test
-    void testMultiply() {
-        assertEquals(6, new MathUtils().multiply(2, 3));
+    public void testAllMethods() {
+        MathUtils math = new MathUtils();
+
+        // Kiểm tra hàm add
+        assertEquals(5, math.add(2, 3), "Hàm add chạy sai rồi!");
+
+        // Kiểm tra hàm subtract (Đảm bảo JaCoCo quét qua dòng này)
+        assertEquals(1, math.subtract(3, 2), "Hàm subtract chạy sai rồi!");
+
+        // Kiểm tra hàm multiply (Đảm bảo JaCoCo quét qua dòng này)
+        assertEquals(6, math.multiply(2, 3), "Hàm multiply chạy sai rồi!");
     }
 }
